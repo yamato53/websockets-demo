c9cabd4ed56ec606d57a3130976dd49288af3e1e
Dakota <dakotakearns@gmail.com>
2019-10-11 16:23:12 -0700

    Added Websocket demo to Github

---
+++ app.js
@@ -0,0 +1,65 @@
+window.onload = function() {
+    // These are all the references to the HTML elements.
+    const form = document.getElementById('message-form');
+    const messageField = document.getElementById('message');
+    const messagesList = document.getElementById('messages');
+    const socketStatus = document.getElementById('status');
+    const closeBtn = document.getElementById('close');
+
+    
+    // Creating the new websocket.
+    const socket = new WebSocket('wss://echo.websocket.org');
+
+    // Displays a message when the websocket is opened.
+    socket.onopen = function(event) {
+        socketStatus.innerHTML = 'Connected to: ' + event.currentTarget.url;
+        socketStatus.className = 'open';
+    };
+
+    // Error collection
+    socket.onerror = (err) => {
+        console.log('WebSocket Error: ' + err);
+    };
+
+    // Handles messages sent by the user to the server
+    form.onsubmit = (submit) => {
+        submit.preventDefault();
+
+        // Gets the message the user inputs to the text field.
+        const message = messageField.value
+
+        // Uses the websocket to send the message
+        socket.send(message);
+
+        // Adds the message to the 'messages' list
+        messagesList.innerHTML += '<li class="sent"><span>Sent:</span>' +
+                            message + '</li>'
+
+        // Clears the message field after use
+        messageField.value = '';
+
+        return false;
+    };
+
+    // handles messages sent from the server to the user
+    socket.onmessage = (event) => {
+        const message = event.data;
+        messagesList.innerHTML += '<li class="received><span>Message received:</span>' +
+                                message + '</li>'
+    };
+
+    // Alert the user that the websocket connection is now closed
+    socket.onclose = (event) => {
+        socketStatus.innerHTML = 'Discconected from the WebSocket server.';
+        socketStatus.className = 'closed';
+    };
+
+    closeBtn.onclick = (clicked) => {
+        clicked.preventDefault();
+
+        // Closes the connection with the websocket.
+        socket.close();
+
+        return false;
+    };
+}
